d3456b27fb6bfcde8e9dee8265e69e118564b41d
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2022-07-30 23:07:09 +0200

    Implanted PTGetSample and PTPatternData

--- Source/main/PTGetSample.c
+++ Source/main/PTGetSample.c
@@ -4,10 +4,24 @@
 
 #include "ptreplay_private.h"
 
+#pragma pack(2)
+/* This structure is returned by GetSample function */
+struct PTSample
+{
+	UBYTE Name[22];		/* 22 Null terminated string with samplename */ 
+	UWORD Length;		/* 02 Sample length in words */
+	UBYTE FineTune;		/* 01 FineTune of sample in lower 4 bits */
+	UBYTE Volume;		/* 01 Volume of sample */ 
+	UWORD Repeat;		/* 02 Repeat start in number of words */ 
+	UWORD Replen;		/* 02 Repeat length in number of words */ 
+// = 30 bytes.
+};
+#pragma pack()
+
 struct PTSample * _PTReplay_PTGetSample(struct PTReplayIFace *Self,
 	struct Module *module, WORD Nr)
 {
-	/* Write me. Really, I dare you! */
-	IExec->DebugPrintF("Function ptreplay::PTGetSample not implemented\n");  
-	return (struct PTSample *)0;
+	struct PTSample *samples = (module -> file_data + 0x14);
+	return samples+Nr;
 }
+
--- Source/main/PTPatternData.c
+++ Source/main/PTPatternData.c
@@ -4,10 +4,16 @@
 
 #include "ptreplay_private.h"
 
+// 4 bytes per channel. 4 channels.
+
+#define row_size 4*4
+#define pattern_offset 1084
+#define pattern_size 1024
+
 APTR _PTReplay_PTPatternData(struct PTReplayIFace *Self,
 	struct Module *module, UBYTE Pattern, UBYTE Row)
 {
-	/* Write me. Really, I dare you! */
-	IExec->DebugPrintF("Function ptreplay::PTPatternData not implemented\n");  
-	return (APTR)0;
+	unsigned char *patterns = module -> file_data + pattern_offset ;
+
+	return (APTR) (patterns + (pattern_size * Pattern) + (row_size * Row));
 }
